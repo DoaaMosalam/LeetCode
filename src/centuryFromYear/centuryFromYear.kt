441b48e8a15b934605d09ee2c03222e74c903a40
Doaa Mosalam <dodoo_mosalem@yahoo.com>
2023-01-30 01:54:45 +0200

    LeetCode solving problems

--- src/centuryFromYear/centuryFromYear.kt
+++ src/centuryFromYear/centuryFromYear.kt
@@ -15,5 +15,4 @@ class centuryFromYear {
        }
 
     }
-
 }
---
+++ src/checkPalindrome/checkPalindrome.kt
@@ -0,0 +1,36 @@
+package checkPalindrome
+/*
+Given the string, check if it is a palindrome.
+
+Example
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+//===========================================
+ --- Palindrome---
+A string that doesn't changed when reversed (it reads the same backward and forward).
+Examples:
+"eye" is a palindrome
+"noon" is a palindrome
+"decaf faced" is a palindrome
+"taco cat" is not a palindrome (backwards it spells "tac ocat")
+"racecars" is not a palindrome (backwards it spells "sracecar")
+*/
+fun main(args:Array<String>){
+    val p =checkPalindrome()
+    //println(p.solution("decaf faced"))
+
+}
+class checkPalindrome {
+    fun solution(inputString: String): Boolean {
+        if (inputString == inputString.reversed()){
+            return true
+        }
+        return false
+    }
+
+
+}
